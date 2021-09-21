e6beaca1db2415584e35fdb0007608ff41e9741e
wenmaowwm <wwmjeff@163.com>
2021-09-21 21:21:11 +0800

    作业

---
+++ src/main/java/com/geek/javaCourse/JavaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.geek.javaCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geek/javaCourse/JavaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.javaCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
