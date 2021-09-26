c497e250e0c1e640a424c9bddd472cb72131ee98
wenmaowwm <wwmjeff@163.com>
2021-09-26 18:32:22 +0800

    week2

---
+++ src/main/java/week2/HttpClientTest.java
@@ -0,0 +1,35 @@
+package week2;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClientTest {
+    public static CloseableHttpClient httpclient = HttpClients.createDefault();
+    // GET 调用
+    public static String getAsString(String url) throws IOException {
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response1 = null;
+        try {
+            response1 = httpclient.execute(httpGet);
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            return EntityUtils.toString(entity1, "UTF-8");
+        } finally {
+            if (null != response1) {
+                response1.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String url = "http://localhost:8808/";
+        String text = getAsString(url);
+        System.out.println("url: " + url + " ; response: \n" + text);
+    }
+}
