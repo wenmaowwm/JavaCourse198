9f71eeaf24966c6f0e6c02fdb5401243a0601ce2
wenmaowwm <wwmjeff@163.com>
2021-10-08 00:07:33 +0800

    week3

---
+++ src/main/java/week3/ProxyBizFilter.java
@@ -0,0 +1,32 @@
+package week3;
+
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
+import week3.io.github.kimmking.gateway.filter.HttpRequestFilter;
+
+public class ProxyBizFilter implements HttpRequestFilter {
+
+
+    public static ProxyBizFilter newInstance() {
+        return new ProxyBizFilter();
+    }
+
+    @Override
+    public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        String uri = fullRequest.uri();
+        System.out.println(" filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx)接收到的请求,url: " + uri);
+        if (uri.startsWith("/test")) {
+            // 放过
+        } else {
+            throw new RuntimeException("不支持的uri:" + uri);
+        }
+        HttpHeaders headers = fullRequest.headers();
+        if (headers == null) {
+            headers = new DefaultHttpHeaders();
+        }
+        headers.add("proxy-tag", this.getClass().getSimpleName());
+    }
+}
